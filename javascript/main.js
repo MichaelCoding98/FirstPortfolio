bbbefc19b5d86cb9169989614b35a399d6f43ef4
Michael <MichaelRabti@wp.pl>
2021-05-05 18:04:17 +0200

    Width display fix

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,8 @@
-const open = document.getElementById('hamburger');
+const burgerBtn = document.getElementById('hamburger');
 const changeIcon = true;
 const navBtn = document.querySelector('.hamburger');
-const lightMain = document.querySelector('.light-main');
-const lightToggle = document.querySelector('.light-dark');
+const lightDark = document.querySelector('.light-dark');
+let lightMain = document.querySelector('.light-main');
 const footerYear = document.querySelector('.year');
 
 const handleCurrentYear = () => {
@@ -11,31 +11,70 @@ const handleCurrentYear = () => {
 };
 handleCurrentYear();
 
-open.addEventListener('click', function () {
+burgerBtn.addEventListener('click', function () {
 	var overlay = document.querySelector('.overlay');
 	var nav = document.querySelector('nav');
-	// var icon = document.querySelector('.menu-toggle i');
 
 	overlay.classList.toggle('menu-open');
 	nav.classList.toggle('menu-open');
-
-	// if (changeIcon) {
-	// 	icon.classList.remove('fa-bars');
-	// 	icon.classList.add('fa-times');
-
-	// 	changeIcon = false;
-	// } else {
-	// 	icon.classList.remove('fa-times');
-	// 	icon.classList.add('fa-bars');
-	// 	changeIcon = true;
-	// }
 });
 
 navBtn.addEventListener('click', () => {
 	navBtn.classList.toggle('is-active');
 });
-
-lightToggle.addEventListener('click', () => {
-	lightToggle.classList.toggle('light-dark-toggle')
+lightDark.addEventListener('click', () => {
 	lightMain.toggleAttribute('disabled');
+    lightDark.classList.toggle('light-dark-toggle')
 });
+
+// const toggleSwitch = document.getElementById('checkbox');
+// const currentTheme = localStorage.getItem('data-theme');
+
+// if (currentTheme) {
+//     document.documentElement.setAttribute('data-theme', currentTheme);
+
+//     if (currentTheme === 'dark') {
+//         toggleSwitch.checked = true;
+//     }
+// }
+
+// function switchTheme(e) {
+//     if (e.target.checked) {
+//         document.documentElement.setAttribute('data-theme', 'dark');
+//         localStorage.setItem('data-theme', 'dark');
+//     } else {
+//         document.documentElement.setAttribute('data-theme', 'light');
+//         localStorage.setItem('data-theme', 'light');
+//     }
+// }
+
+// toggleSwitch.addEventListener('change', switchTheme, false);
+
+// const lightToggle = document.querySelector('.light-dark');
+// let lightMain = document.querySelector('.light-main');
+// let darkMode = localStorage.getItem('light-main');
+
+// const enableDarkMode = () => {
+// 	lightToggle.classList.add('light-dark-toggle');
+
+// 	localStorage.setItem('darkMode', null);
+
+// };
+
+// const disableDarkMode = () => {
+// 	lightToggle.classList.remove('light-dark-toggle');
+
+// 	localStorage.setItem('darkMode', 'enabled');
+// };
+
+// lightToggle.addEventListener('click', () => {
+// 	darkMode = localStorage.getItem('darkMode');
+
+// 	if (darkMode === 'enabled') {
+// 		enableDarkMode();
+// 		lightMain.setAttribute('disabled', true);
+// 	} else if (darkMode === null) {
+// 		disableDarkMode();
+// 		lightMain.setAttribute('disabled', false);
+// 	}
+// });
